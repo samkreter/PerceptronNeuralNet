755feaddd298fc5cfeb9cd260035cf8b0f34a17e
Sam Kreter <samkreter@gmail.com>
2016-09-14 19:25:48 -0500

    building single perceptron

--- test.py
+++ test.py
@@ -1,10 +1,33 @@
 import numpy as np
 
 
-sigmoid = lambda x: 1 / (1 + np.exp(-x))
 
-x = np.array([0.05,0.1,1])
-w = np.array([.15,.2,.35])
 
-print(w.dot(x))
+
+
+# x = np.array([0.05,0.1,1])
+# w = np.array([.15,.2,.35])
+
+# print(sigmoid(w.dot(x)))
+
+class Nueron:
+    """docstring for Nueron"""
+    def __init__(self,bias,numInputs):
+        self.bias = bias
+        self.weights = np.append(self.randomWeights(numInputs),bias)
+
+    def sigmoid(self,x):
+        print(x)
+        return 1 / (1 + np.exp(-x))
+
+    def output(self,x):
+        x = np.append(x,1)
+        print(self.weights)
+        return (self.sigmoid(x.dot(self.weights)))
+
+    def randomWeights(self,num):
+        return np.random.uniform(low=0.5, high=13.3, size=(num))
+
+
+
 
