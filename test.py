6194857f2f4d5080f42c30bf45dde7b1bf3cbade
Sam Kreter <samkreter@gmail.com>
2016-09-19 19:01:53 -0500

    finally got the nueral net working for sample output

--- test.py
+++ test.py
@@ -18,16 +18,6 @@ class NueralNet():
         self.setInputToHiddenWeights(hWeights)
         self.setHiddenToOutput(outWeights)
 
-    def inspect(self):
-        print('------')
-        print('* Inputs: {}'.format(self.numInputs))
-        print('------')
-        print('Hidden Layer')
-        self.hiddenLayer.inspect()
-        print('------')
-        print('* Output Layer')
-        self.outputLayer.inspect()
-        print('------')
 
 
     def train(self,inputs, labels):
@@ -53,13 +43,14 @@ class NueralNet():
         #Adjust output layer weights
         for i in range(len(self.outputLayer.nuerons)):
             for w in range(len(self.outputLayer.nuerons[i].weights)):
+                # print("output: ",self.learningRate * self.outputLayer.nuerons[i].getInputAtIndex(i) * outputGrad[i])
                 self.outputLayer.nuerons[i].weights[w] -= self.learningRate * self.outputLayer.nuerons[i].getInputAtIndex(i) * outputGrad[i]
 
         #Adjust hidden layer weights
         for i in range(len(self.hiddenLayer.nuerons)):
             for w in range(len(self.hiddenLayer.nuerons[i].weights)):
                 self.hiddenLayer.nuerons[i].weights[w] -= self.learningRate * hiddenGrad[i] * self.hiddenLayer.nuerons[i].getInputAtIndex(i)
-
+                # print("hidden: ",self.learningRate * hiddenGrad[i] * self.hiddenLayer.nuerons[i].getInputAtIndex(i))
 
 
     def fullForwardPass(self, inputs):
@@ -70,9 +61,9 @@ class NueralNet():
 
         for nueron in self.hiddenLayer.nuerons:
             if hWeights:
-                nueron.weights = np.append(hWeights,nueron.bias)
+                nueron.weights = hWeights[:self.numInputs]
             else:
-                nueron.weights = np.append(np.random.rand(self.numInputs),nueron.bias)
+                nueron.weights = np.random.rand(self.numInputs)
 
 
 
@@ -82,7 +73,7 @@ class NueralNet():
         for nueron in self.outputLayer.nuerons:
 
             if outWeights:
-                nueron.weights = outWeights
+                nueron.weights = outWeights[:self.numInputs]
             else:
                 nueron.weight = np.random.rand(len(self.hiddenLayer.nuerons))
 
@@ -114,17 +105,7 @@ class Layer():
         for i in range(numNuerons):
             self.nuerons.append(Nueron(self.bias))
 
-    def inspect(self):
-        print('Neurons:', len(self.nuerons))
-        for n in range(len(self.nuerons)):
 
-            print(' Neuron', n)
-
-            for w in range(len(self.nuerons[n].weights)):
-
-                print('  Weight:', self.nuerons[n].weights[w])
-
-            print('  Bias:', self.bias)
 
     def forwardPass(self,input):
         outputs = []
@@ -144,7 +125,6 @@ class Nueron:
 
 
     def sigmoid(self,x):
-        print(x)
         return 1 / (1 + np.exp(-x))
 
     def getOutput(self,inputs):
@@ -177,9 +157,9 @@ class Nueron:
 #learningRate,numInputs,numHidden,numOutputs
 nn = NueralNet(0.5,2, 2, 2, hWeights=[0.15, 0.2, 0.25, 0.3], hBias=0.35, outputBias=0.6, outWeights=[0.4, 0.45, 0.5, 0.55])
 
-nn.inspect();
-# for i in range(10000):
+#nn.inspect();
+for i in range(10000):
 
-#     nn.train([0.05, 0.1], [0.01, 0.99])
+    nn.train([0.05, 0.1], [0.01, 0.99])
 
-#     print(i, round(nn.getOverallError([[[0.05, 0.1], [0.01, 0.99]]]), 9))
+    print(i, round(nn.getOverallError([[[0.05, 0.1], [0.01, 0.99]]]), 9))
